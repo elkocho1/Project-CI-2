c6dedc3b6c2175e3514cdfa0785351ba7dba3711
Michael <michaell.hirt@gmail.com>
2023-12-14 18:11:26 -0500

    add basic logic for click event choice buttons

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,17 @@ const modalOne = document.querySelector(".modal-overlay");
 startBtn.addEventListener("click", function () {
     modalOne.classList.add("open-overlay");
 });
+
+// select the button choices and score container
+// when one button is click the pop up closes and the score container is displayed
+
+const choiceButtons = document.querySelectorAll(".btn-choices button");
+const scoreContainer = document.querySelector(".score-container");
+
+//function to handle choice button click event
+//Remove open-overlay class 
+modalOne.classList.remove("open-overlay");
+//Display the score container
+scoreContainer.computedStyleMap.display = "block";
+// Add event listener to each choice button
+// For Loop with click event listener 
